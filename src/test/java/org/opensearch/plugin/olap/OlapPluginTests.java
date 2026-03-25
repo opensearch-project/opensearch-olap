e10c9f133e69fa41a53afc85714f13dd13dce1d3
Lantao Jin <ltjin@amazon.com>
2026-03-25 08:33:17 +0000

    remove local jars
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

---
+++ src/test/java/org/opensearch/plugin/olap/OlapPluginTests.java
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.plugin.olap;
+
+import org.opensearch.test.OpenSearchTestCase;
+
+public class OlapPluginTests extends OpenSearchTestCase {
+
+    public void testPluginInstantiation() {
+        OlapPlugin plugin = new OlapPlugin();
+        assertNotNull(plugin);
+    }
+}
